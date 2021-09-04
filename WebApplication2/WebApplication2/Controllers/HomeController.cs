227e6eda08c5a9719d0d45f6b85d1e33cdf9f985
Eduardo Faciano <edu21.ef@gmail.com>
2021-09-03 22:06:10 -0300

    Tp Completo

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication2.Models;
+using static WebAPI_MVC.Models.Api;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public int Punto1(int numero)
+        {
+            return Convert.ToInt32(Math.Pow(numero, 2));
+        }
+
+        public string Punto2(int dividendo, int divisor)
+        {
+            try
+            {
+                if (divisor == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                return Convert.ToString(dividendo / divisor);
+            }
+            catch (DivideByZeroException ex2)
+            {
+                return "Se produjo un error de división por cero - " + ex2.Message.ToString();
+            }
+            catch (Exception ex1)
+            {
+                return "Se produjo un error desconocido - " + ex1.Message.ToString();
+            }
+        }
+        public string Punto3()
+        {
+            try
+            {
+                string cadena = "";
+                List<Provincia> ListadoDeProvincias = ConsultaApi();
+                foreach (var prov in ListadoDeProvincias)
+                {
+                    cadena += $"id: {prov.id}. Provincia: {prov.nombre} \n";
+                }
+                return cadena;
+            }
+            catch (Exception ex1)
+            {
+                _logger.LogError(ex1.Message);
+                return "Error: " + ex1.Message.ToString();
+
+            }
+        }
+        public string Punto4(string kilometros, string litros)
+        {
+            float resultado, km, l;
+            string mensaje;
+
+            try
+            {
+                if (kilometros != null && litros != null)
+                {
+                    km = float.Parse(kilometros, System.Globalization.NumberStyles.Float);
+                    l = float.Parse(litros, System.Globalization.NumberStyles.Float);
+
+                    if (l != 0)
+                    {
+                        resultado = km / l;
+                        mensaje = resultado.ToString();
+                    }
+                    else
+                    {
+                        mensaje = "No se puede dividir en 0";
+                    }
+                }
+                else
+                {
+                    mensaje = "Ingrese los datos antes de calcular";
+                }
+            }
+            catch (FormatException ex)
+            {
+                mensaje = "Los datos ingresados no son numeros o no están en el formato correcto";
+                _logger.LogError(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                mensaje = "Valores ingresados fuera de los rangos esperados";
+                _logger.LogError(ex.ToString());
+            }
+
+            return mensaje;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/APIController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace WebAPI_MVC.Models
+{
+    public class Api
+    {
+        public static List<Provincia> ConsultaApi()
+        {
+            var url = $"https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            ProvArg provincias = null;
+
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader != null)
+                        {
+                            using (StreamReader objReader = new StreamReader(strReader))
+                            {
+                                string responseBody = objReader.ReadToEnd();
+                                provincias = JsonSerializer.Deserialize<ProvArg>(responseBody);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                throw new Exception("Error de conexion con la Api");
+            }
+
+            return provincias.provincias;
+
+        }
+
+        public class Parametros
+        {
+            public List<string> campos { get; set; }
+        }
+
+        public class Provincia
+        {
+            public string id { get; set; }
+
+            public string nombre { get; set; }
+        }
+
+        public class ProvArg
+        {
+            public int cantidad { get; set; }
+
+            public int inicio { get; set; }
+
+            public Parametros parametros { get; set; }
+
+            public List<Provincia> provincias { get; set; }
+
+            public int total { get; set; }
+        }
+    }
+}
